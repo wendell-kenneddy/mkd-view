3b8b40a736ef6a09711298ef53f9ea8ebd8d0d76
Wendell Kenneddy <wkenneddy505@gmail.com>
2022-05-09 16:55:47 -0300

    refactor(homepage): update description related meta tags

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,18 +6,18 @@ export default function Home() {
   return (
     <>
       <Head>
-        <meta name="description" content="A simple Markdown viewer." />
+        <meta name="description" content="A simple Markdown editor." />
         <meta property="og:type" content="website" />
         <meta property="og:title" content="MKDview" />
         <meta property="og:site_name" content="MKDview" />
-        <meta property="og:description" content="A simple Markdown viewer." />
+        <meta property="og:description" content="A simple Markdown editor." />
         <meta property="og:url" content="https://mkdview.vercel.app/" />
         <meta property="og:locale" content="en-US" />
         <meta property="twitter:card" content="summary" />
         <meta property="twitter:title" content="MKDview" />
         <meta
           property="twitter:description"
-          content="A simple Markdown viewer."
+          content="A simple Markdown editor."
         />
         <meta property="twitter:url" content="https://mkdview.vercel.app/" />
         <title>MKDview</title>
