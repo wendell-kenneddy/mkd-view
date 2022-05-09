ea375a46f6c54838df41ebfd2ff6b3ce4e9d208a
Wendell Kenneddy <wkenneddy505@gmail.com>
2022-05-09 15:43:42 -0300

    feat(logo_comp): add link to home

--- src/components/Logo/index.tsx
+++ src/components/Logo/index.tsx
@@ -1,5 +1,19 @@
+import { Anchor } from '@mantine/core';
 import Image from 'next/image';
+import Link from 'next/link';
 
 export function Logo() {
-  return <Image src="/logo.svg" alt="Logo" width={77} height={16} />;
+  return (
+    <Link href="/" passHref>
+      <Anchor
+        sx={() => ({
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center'
+        })}
+      >
+        <Image src="/logo.svg" alt="Logo" width={77} height={16} />
+      </Anchor>
+    </Link>
+  );
 }
