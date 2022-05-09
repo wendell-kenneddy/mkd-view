2a1376c24b0c9ebc5f8f1a2a76afa3eb208a5620
Wendell Kenneddy <wkenneddy505@gmail.com>
2022-05-09 15:27:55 -0300

    feat(tabs_comp): use OpenFileButton

--- src/components/Tabs/index.tsx
+++ src/components/Tabs/index.tsx
@@ -5,45 +5,50 @@ import ReactMarkdown from 'react-markdown';
 import RemarkGFM from 'remark-gfm';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { materialOceanic } from 'react-syntax-highlighter/dist/cjs/styles/prism';
+import { OpenFileButton } from '../OpenFileButton';
 
 export function Tabs() {
   const [markdown, setMarkdown] = useState('');
 
   return (
-    <MantineTabs color="orange" grow>
-      <MantineTabs.Tab label="Markdown">
-        <MarkdownTextArea
-          onChange={(value) => setMarkdown(value)}
-          value={markdown}
-        />
-      </MantineTabs.Tab>
+    <>
+      <MantineTabs color="orange" grow>
+        <MantineTabs.Tab label="Markdown">
+          <MarkdownTextArea
+            onChange={(value) => setMarkdown(value)}
+            value={markdown}
+          />
+        </MantineTabs.Tab>
 
-      <MantineTabs.Tab label="Preview">
-        <ReactMarkdown
-          remarkPlugins={[RemarkGFM]}
-          components={{
-            code({ node, inline, className, children, ...props }) {
-              const match = /language-(\w+)/.exec(className || '');
-              return !inline && match ? (
-                <SyntaxHighlighter
-                  style={materialOceanic}
-                  language={match[1]}
-                  PreTag="div"
-                  {...props}
-                >
-                  {String(children).replace(/\n$/, '')}
-                </SyntaxHighlighter>
-              ) : (
-                <code className={className} {...props}>
-                  {children}
-                </code>
-              );
-            }
-          }}
-        >
-          {markdown}
-        </ReactMarkdown>
-      </MantineTabs.Tab>
-    </MantineTabs>
+        <MantineTabs.Tab label="Preview">
+          <ReactMarkdown
+            remarkPlugins={[RemarkGFM]}
+            components={{
+              code({ node, inline, className, children, ...props }) {
+                const match = /language-(\w+)/.exec(className || '');
+                return !inline && match ? (
+                  <SyntaxHighlighter
+                    style={materialOceanic}
+                    language={match[1]}
+                    PreTag="div"
+                    {...props}
+                  >
+                    {String(children).replace(/\n$/, '')}
+                  </SyntaxHighlighter>
+                ) : (
+                  <code className={className} {...props}>
+                    {children}
+                  </code>
+                );
+              }
+            }}
+          >
+            {markdown}
+          </ReactMarkdown>
+        </MantineTabs.Tab>
+      </MantineTabs>
+
+      <OpenFileButton onFileRead={(text) => setMarkdown(text)} />
+    </>
   );
 }
