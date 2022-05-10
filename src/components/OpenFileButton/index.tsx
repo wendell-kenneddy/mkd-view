32faa8d51a057aaca4ef25ba910e69383c5511bc
Wendell Kenneddy <wkenneddy505@gmail.com>
2022-05-10 14:46:00 -0300

    feat(openfilebutton_comp): add validation to wrong file formats

--- src/components/OpenFileButton/index.tsx
+++ src/components/OpenFileButton/index.tsx
@@ -1,4 +1,5 @@
 import { Button } from '@mantine/core';
+import { cleanNotifications, showNotification } from '@mantine/notifications';
 import { useRef } from 'react';
 
 interface OpenFileButtonProps {
@@ -8,6 +9,16 @@ interface OpenFileButtonProps {
 export function OpenFileButton({ onFileRead }: OpenFileButtonProps) {
   const inputRef = useRef<HTMLInputElement>(null);
 
+  function showToast() {
+    cleanNotifications();
+    showNotification({
+      title: 'Invalid file type',
+      message: 'Only Markdown files (.md) are accepted.',
+      color: 'red',
+      autoClose: 3000
+    });
+  }
+
   function handleOpenFile() {
     inputRef.current && inputRef.current.click();
   }
@@ -16,6 +27,9 @@ export function OpenFileButton({ onFileRead }: OpenFileButtonProps) {
     if (!files) return;
 
     const file = files[0];
+
+    if (file.type !== 'text/markdown') return showToast();
+
     const reader = new FileReader();
     reader.onload = (e) => onFileRead(String(e.target?.result));
     reader.readAsText(file);
@@ -29,7 +43,6 @@ export function OpenFileButton({ onFileRead }: OpenFileButtonProps) {
         hidden
         ref={inputRef}
         onChange={(e) => handleReadFile(e.target.files)}
-        accept=".md,text/plain"
       />
 
       <Button color="orange" my="md" onClick={handleOpenFile}>
